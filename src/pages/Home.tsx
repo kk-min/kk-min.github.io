0cda89eb17f28d95ecdefec13a38a0fb91f67162
MINK <76023265+mink0003@users.noreply.github.com>
2022-09-21 13:13:47 +0800

    Add welcome message with animation

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+//Typescript functional react component:
+import React from 'react';
+import '../css/Home.css';
+
+export default function Home() {
+	return (
+		<div className="home-container">
+			<div className='welcome-message'>Welcome</div>
+		</div>
+	);
+}
