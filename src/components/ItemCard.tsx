2ad2e83939d5ad68dd8c6ddb3ee8dde66ec8afdc
mink0003 <76023265+kk-min@users.noreply.github.com>
2022-09-23 23:15:51 +0800

    Add selection state management for Project tab

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -6,15 +6,16 @@ export interface PropTypes {
     itemName: string;
     bodyText: string;
     bottomText: string;
+    onClickHandler: () => void;
 }
 
 export function ItemCard(props: PropTypes) {
-    const onClickHandler = () => {
-        alert('Test! :)');
-    };
-
     return (
-        <div id={props.itemName} className='item-card' onClick={onClickHandler}>
+        <div
+            id={props.itemName}
+            className='item-card'
+            onClick={props.onClickHandler}
+        >
             <img className='card-image' src={props.image} />
             <h2 className='card-title'>{props.itemName}</h2>
             <h4 className='card-description'>{props.bodyText}</h4>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,35 +4,53 @@ import { ItemCard } from './ItemCard';
 
 export default function Projects() {
     const [selection, setSelection] = React.useState('all');
-
-    return (
-        <div className='content'>
-            <div className='title-container'>
-                <h1 className='tab-title'>Projects</h1>
-                <a href='https://github.com/kk-min'>
-                    <img className='github-logo' src={github_logo} />
-                </a>
+    if (selection === 'all') {
+        return (
+            <div className='content'>
+                <div className='title-container'>
+                    <h1 className='tab-title'>Projects</h1>
+                    <a href='https://github.com/kk-min'>
+                        <img className='github-logo' src={github_logo} />
+                    </a>
+                </div>
+                <div className='card-container'>
+                    <ItemCard
+                        image=''
+                        itemName='Frontend'
+                        bodyText='Web development'
+                        bottomText='React, TypeScript, Javascript, CSS'
+                        onClickHandler={() =>
+                            setSelection((prev) => 'frontend')
+                        }
+                    />
+                    <ItemCard
+                        image=''
+                        itemName='Backend'
+                        bodyText='Android / Database / Networking'
+                        bottomText='Java, Kotlin, C/C++'
+                        onClickHandler={() => setSelection((prev) => 'backend')}
+                    />
+                    <ItemCard
+                        image=''
+                        itemName='Miscellaneous'
+                        bodyText='Bits and pieces'
+                        bottomText='Python, VBA'
+                        onClickHandler={() =>
+                            setSelection((prev) => 'miscellaneous')
+                        }
+                    />
+                </div>
             </div>
-            <div className='card-container'>
-                <ItemCard
-                    image=''
-                    itemName='Frontend'
-                    bodyText='Web development'
-                    bottomText='React, TypeScript, Javascript, CSS'
-                />
-                <ItemCard
-                    image=''
-                    itemName='Backend'
-                    bodyText='Android / Database / Networking'
-                    bottomText='Java, Kotlin, C/C++'
-                />
-                <ItemCard
-                    image=''
-                    itemName='Miscellaneous'
-                    bodyText='Bits and pieces'
-                    bottomText='Python, VBA'
-                />
+        );
+    } else if (selection === 'frontend') {
+        return (
+            <div className='content'>
+                <div className='title-container'>
+                    <h1 className='tab-title'>Frontend</h1>
+                </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return <div>Loading</div>;
+    }
 }
