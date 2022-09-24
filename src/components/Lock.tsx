4d5bead9618ae8abb494e91582e915ede62025c5
MINK <76023265+kk-min@users.noreply.github.com>
2022-09-24 22:39:32 +0800

    Add secret generator function

--- src/components/Lock.tsx
+++ src/components/Lock.tsx
@@ -1,10 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import padlock_default from '../assets/padlock_default.png';
 import padlock_open from '../assets/padlock_open.png';
 import padlock_error from '../assets/padlock_error.png';
 
+const randomizerArray = ['1', '2', '3', '4'];
+
+const getRandomSecret = (sourceArray: string[]) => {
+	let secret: string = '';
+	let tempArray = [...sourceArray];
+	while (tempArray.length > 0) {
+		let index = Math.floor(Math.random() * tempArray.length);
+		let randomItem = tempArray[index];
+		secret += randomItem;
+		tempArray.splice(index, 1); // Remove selected item from array
+	}
+	return secret;
+};
+
 export default function Lock() {
 	const [lockState, setLockState] = useState('locked');
+
+	useEffect(() => {
+		const secret = getRandomSecret(randomizerArray);
+	}, []);
 
 	const onClickHandler = () => {
 		const lockElement = document.getElementById('lock');
@@ -15,6 +33,7 @@ export default function Lock() {
 			}, 300);
 		}
 	};
+
 	return (
 		<div className='lock-container'>
 			<img
