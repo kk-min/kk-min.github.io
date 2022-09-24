cb4744e43b6f1f1cbe3cfdf9665a2c555b103cba
MINK <76023265+kk-min@users.noreply.github.com>
2022-09-24 18:59:59 +0800

    Update Backend and Miscellaneous sections of Project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { getParseTreeNode } from 'typescript';
 import github_logo from '../assets/github_logo.png';
 import { ItemCard } from './ItemCard';
@@ -10,8 +10,21 @@ import project_data from '../data/project_data.json';
 
 export default function Projects() {
 	const [selection, setSelection] = React.useState('all');
+
+	const githubLogo = useMemo(() => {
+		return (
+			<img
+				className='github-logo'
+				src={github_logo}
+				alt='Github'
+				title='Github'
+			/>
+		);
+	}, [github_logo]);
+
 	// Hide horizontal scrollbar:
 	document.body.style.overflowX = 'hidden';
+
 	if (selection === 'all') {
 		return (
 			<div className='content'>
@@ -22,12 +35,7 @@ export default function Projects() {
 						target='_blank'
 						rel='noopener noreferrer'
 					>
-						<img
-							className='github-logo'
-							src={github_logo}
-							alt='Github'
-							title='Github'
-						/>
+						{githubLogo}
 					</a>
 				</div>
 				<div className='card-container'>
@@ -90,17 +98,58 @@ export default function Projects() {
 	} else if (selection === 'backend') {
 		return (
 			<div className='content'>
-				<div className='title-container'>
-					<h1 className='tab-title'>Backend</h1>
+				<div
+					className='back-button'
+					onClick={() => setSelection('all')}
+				>
+					⮌
 				</div>
+				<div className='title-container'>
+					<h1 className='tab-title'>
+						Backend <img src={gears} style={{ height: '6vh' }} />
+					</h1>
+				</div>
+				{project_data.backend.map((data) => {
+					return (
+						<ProjectItem
+							projectTitle={data.name}
+							projectDescription={data.description}
+							projectIcon={data.icon}
+							projectRole={data.subheader}
+							projectLink={data.link}
+							projectBulletPoints={data.bulletpoints}
+						/>
+					);
+				})}
 			</div>
 		);
 	} else if (selection === 'miscellaneous') {
 		return (
 			<div className='content'>
-				<div className='title-container'>
-					<h1 className='tab-title'>Miscellaneous</h1>
+				<div
+					className='back-button'
+					onClick={() => setSelection('all')}
+				>
+					⮌
 				</div>
+				<div className='title-container'>
+					<h1 className='tab-title'>
+						Miscellaneous{' '}
+						<img src={pencil_case} style={{ height: '6vh' }} />
+					</h1>
+				</div>
+				{project_data.miscellaneous.map((data) => {
+					return (
+						<ProjectItem
+							projectTitle={data.name}
+							projectDescription={data.description}
+							projectIcon={data.icon}
+							projectRole={data.subheader}
+							projectLink={data.link}
+							projectBulletPoints={data.bulletpoints}
+						/>
+					);
+				})}
 			</div>
 		);
 	} else {
