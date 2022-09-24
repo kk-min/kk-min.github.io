fbe0d00a7a67245682dc847cc8003d6c5d6806b8
MINK <76023265+kk-min@users.noreply.github.com>
2022-09-24 22:15:42 +0800

    Add Lock component and shake animation

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Lock from './Lock';
 
 export default function Contact() {
-    return (
-        <div className='content'>
-            <h1 className='tab-title'>Contact</h1>
-        </div>
-    );
+	return (
+		<div className='content'>
+			<h1 className='tab-title'>Contact</h1>
+			<Lock />
+		</div>
+	);
 }
---
+++ src/components/Lock.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import padlock_default from '../assets/padlock_default.png';
+import padlock_open from '../assets/padlock_open.png';
+import padlock_error from '../assets/padlock_error.png';
+
+export default function Lock() {
+	const [lockState, setLockState] = useState('locked');
+
+	const onClickHandler = () => {
+		const lockElement = document.getElementById('lock');
+		if (lockElement) {
+			lockElement.classList.add('shaking');
+			setTimeout(() => {
+				lockElement.classList.remove('shaking');
+			}, 300);
+		}
+	};
+	return (
+		<div className='lock-container'>
+			<img
+				id='lock'
+				className='lock'
+				src={padlock_default}
+				onClick={onClickHandler}
+			></img>
+		</div>
+	);
+}
