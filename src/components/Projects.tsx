846c5df1ae65c76d8ac46f3be949e5f159f8e695
MINK <76023265+kk-min@users.noreply.github.com>
2022-09-24 10:18:34 +0800

    Adjust positioning for intro-container and Project contents

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,54 +3,59 @@ import github_logo from '../assets/github_logo.png';
 import { ItemCard } from './ItemCard';
 
 export default function Projects() {
-    const [selection, setSelection] = React.useState('all');
-    if (selection === 'all') {
-        return (
-            <div className='content'>
-                <div className='title-container'>
-                    <h1 className='tab-title'>Projects</h1>
-                    <a href='https://github.com/kk-min'>
-                        <img className='github-logo' src={github_logo} />
-                    </a>
-                </div>
-                <div className='card-container'>
-                    <ItemCard
-                        image=''
-                        itemName='Frontend'
-                        bodyText='Web development'
-                        bottomText='React, TypeScript, Javascript, CSS'
-                        onClickHandler={() =>
-                            setSelection((prev) => 'frontend')
-                        }
-                    />
-                    <ItemCard
-                        image=''
-                        itemName='Backend'
-                        bodyText='Android / Database / Networking'
-                        bottomText='Java, Kotlin, C/C++'
-                        onClickHandler={() => setSelection((prev) => 'backend')}
-                    />
-                    <ItemCard
-                        image=''
-                        itemName='Miscellaneous'
-                        bodyText='Bits and pieces'
-                        bottomText='Python, VBA'
-                        onClickHandler={() =>
-                            setSelection((prev) => 'miscellaneous')
-                        }
-                    />
-                </div>
-            </div>
-        );
-    } else if (selection === 'frontend') {
-        return (
-            <div className='content'>
-                <div className='title-container'>
-                    <h1 className='tab-title'>Frontend</h1>
-                </div>
-            </div>
-        );
-    } else {
-        return <div>Loading</div>;
-    }
+	const [selection, setSelection] = React.useState('all');
+	if (selection === 'all') {
+		return (
+			<div className='content'>
+				<div className='title-container'>
+					<h1 className='tab-title'>Projects</h1>
+					<a href='https://github.com/kk-min'>
+						<img
+							className='github-logo'
+							src={github_logo}
+							alt='Github'
+							title='Github'
+						/>
+					</a>
+				</div>
+				<div className='card-container'>
+					<ItemCard
+						image=''
+						itemName='Frontend'
+						bodyText='Web development'
+						bottomText='React, TypeScript, Javascript, CSS'
+						onClickHandler={() =>
+							setSelection((prev) => 'frontend')
+						}
+					/>
+					<ItemCard
+						image=''
+						itemName='Backend'
+						bodyText='Android / Database / Networking'
+						bottomText='Java, Kotlin, C/C++'
+						onClickHandler={() => setSelection((prev) => 'backend')}
+					/>
+					<ItemCard
+						image=''
+						itemName='Miscellaneous'
+						bodyText='Bits and pieces'
+						bottomText='Python, VBA'
+						onClickHandler={() =>
+							setSelection((prev) => 'miscellaneous')
+						}
+					/>
+				</div>
+			</div>
+		);
+	} else if (selection === 'frontend') {
+		return (
+			<div className='content'>
+				<div className='title-container'>
+					<h1 className='tab-title'>Frontend</h1>
+				</div>
+			</div>
+		);
+	} else {
+		return <div>Loading</div>;
+	}
 }
