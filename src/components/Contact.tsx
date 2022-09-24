45ab9db81658fbdcb763be4339958e56e0c910d8
MINK <76023265+kk-min@users.noreply.github.com>
2022-09-25 01:11:34 +0800

    Add disabling of reset button while animation is running

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -20,10 +20,23 @@ export default function Contact() {
 	const [secret, setSecret] = useState('');
 	const [input, setInput] = useState('');
 	const [lockState, setLockState] = useState('default');
+	const [isAnimating, setIsAnimating] = useState(true);
 
 	useEffect(() => {
 		setSecret((prev) => getRandomSecret(randomizerArray));
+		setTimeout(() => {
+			setIsAnimating(false);
+		}, 5000);
 	}, []);
+
+	useEffect(() => {
+		const resetElement = document.getElementById('reset-button');
+		if (!isAnimating) {
+			if (resetElement) {
+				resetElement.className = 'reset-button enabled';
+			}
+		}
+	}, [isAnimating]);
 
 	const addInput = (input: string) => {
 		setInput((prev) => prev + input);
@@ -32,21 +45,31 @@ export default function Contact() {
 	const resetHandler = () => {
 		const resetElement = document.getElementById('reset-button');
 		if (resetElement) {
+			if (resetElement.className === 'reset-button disabled') {
+				return;
+			}
+
 			resetElement.classList.add('rotate');
+			resetElement.classList.remove('enabled');
+			resetElement.classList.add('disabled');
+
 			setSecret((prev) => getRandomSecret(randomizerArray));
 			setInput('');
 			setLockState('default');
+			setIsAnimating((prev) => true);
 		}
 		setTimeout(() => {
 			resetElement?.classList.remove('rotate');
-		}, 1500);
+			resetElement?.classList.remove('disabled');
+			setIsAnimating((prev) => false);
+		}, 5000);
 	};
 
 	return (
 		<div className='content'>
 			<div
 				id='reset-button'
-				className='reset-button'
+				className='reset-button disabled'
 				onClick={resetHandler}
 			>
 				⭯
