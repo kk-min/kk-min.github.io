0189a9991f0c66862a16ea9a2168276bb0813175
mink0003 <76023265+kk-min@users.noreply.github.com>
2022-09-25 08:00:44 +0800

    Add key to mapped items in Unlocker

--- src/components/Lock.tsx
+++ src/components/Lock.tsx
@@ -4,55 +4,55 @@ import padlock_open from '../assets/padlock_open.png';
 import padlock_error from '../assets/padlock_error.png';
 
 export interface PropTypes {
-	secret: string;
-	input: string;
-	lockState: string;
-	setLockState: React.Dispatch<React.SetStateAction<string>>;
-	success: boolean;
+    secret: string;
+    input: string;
+    lockState: string;
+    setLockState: React.Dispatch<React.SetStateAction<string>>;
+    success: boolean;
 }
 
 export default function Lock(props: PropTypes) {
-	const onClickHandler = () => {
-		const lockElement = document.getElementById('lock');
-		if (lockElement) {
-			// Input not completely filled
-			if (props.input.length < props.secret.length) {
-				lockElement.classList.add('shaking');
-				setTimeout(() => {
-					lockElement.classList.remove('shaking');
-				}, 300);
-			} else if (props.input !== props.secret) {
-				// Input is incorrect
-				lockElement.className = 'lock-failed';
-				props.setLockState('error');
-			} else {
-				// Input completely filled and correct
-				props.setLockState('open');
-			}
-		}
-	};
+    const onClickHandler = () => {
+        const lockElement = document.getElementById('lock');
+        if (lockElement) {
+            // Input not completely filled
+            if (props.input.length < props.secret.length) {
+                lockElement.classList.add('shaking');
+                setTimeout(() => {
+                    lockElement.classList.remove('shaking');
+                }, 300);
+            } else if (props.input !== props.secret) {
+                // Input is incorrect
+                lockElement.className = 'lock-failed';
+                props.setLockState('error');
+            } else {
+                // Input completely filled and correct
+                props.setLockState('open');
+            }
+        }
+    };
 
-	return (
-		<div className='lock-container'>
-			<img
-				id='lock'
-				className={
-					'lock' +
-					(props.lockState === 'error'
-						? '-failed'
-						: props.success
-						? '-exit'
-						: '')
-				}
-				src={
-					props.lockState === 'default'
-						? padlock_default
-						: props.lockState === 'open'
-						? padlock_open
-						: padlock_error
-				}
-				onClick={onClickHandler}
-			></img>
-		</div>
-	);
+    return (
+        <div className='lock-container'>
+            <img
+                id='lock'
+                className={
+                    'lock' +
+                    (props.lockState === 'error'
+                        ? '-failed'
+                        : props.success
+                        ? '-exit'
+                        : '')
+                }
+                src={
+                    props.lockState === 'default'
+                        ? padlock_default
+                        : props.lockState === 'open'
+                        ? padlock_open
+                        : padlock_error
+                }
+                onClick={onClickHandler}
+            ></img>
+        </div>
+    );
 }
--- src/components/Unlocker.tsx
+++ src/components/Unlocker.tsx
@@ -2,86 +2,87 @@ import React, { useEffect, useState } from 'react';
 import { isPropertySignature } from 'typescript';
 
 export interface PropTypes {
-	secret: string;
-	addInput: (input: string) => void;
-	success: boolean;
+    secret: string;
+    addInput: (input: string) => void;
+    success: boolean;
 }
 
 const unlockers = ['1', '2', '3', '4'];
 const timer = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 const animateAnswer = async (secret: string) => {
-	resetUnlockers('disabled');
-	await timer(1000);
-	// Fill the unlocker items in sequence of the secret:
-	for (const c of secret) {
-		const unlockerElement = document.getElementById('unlocker' + c);
-		if (unlockerElement) {
-			unlockerElement.className = 'unlocker-item-filled';
-			await timer(1000);
-		}
-	}
-	resetUnlockers('enabled');
+    resetUnlockers('disabled');
+    await timer(1000);
+    // Fill the unlocker items in sequence of the secret:
+    for (const c of secret) {
+        const unlockerElement = document.getElementById('unlocker' + c);
+        if (unlockerElement) {
+            unlockerElement.className = 'unlocker-item-filled';
+            await timer(1000);
+        }
+    }
+    resetUnlockers('enabled');
 };
 
 const resetUnlockers = (status: string) => {
-	// Un-fill the unlocker items
-	for (const c of unlockers) {
-		const unlockerElement = document.getElementById('unlocker' + c);
-		if (unlockerElement) {
-			unlockerElement.className = 'unlocker-item ' + status;
-		}
-	}
+    // Un-fill the unlocker items
+    for (const c of unlockers) {
+        const unlockerElement = document.getElementById('unlocker' + c);
+        if (unlockerElement) {
+            unlockerElement.className = 'unlocker-item ' + status;
+        }
+    }
 };
 
 const fillUnlockers = () => {
-	for (const c of unlockers) {
-		const unlockerElement = document.getElementById('unlocker' + c);
-		if (unlockerElement) {
-			unlockerElement.className = 'unlocker-item-filled';
-		}
-	}
+    for (const c of unlockers) {
+        const unlockerElement = document.getElementById('unlocker' + c);
+        if (unlockerElement) {
+            unlockerElement.className = 'unlocker-item-filled';
+        }
+    }
 };
 
 export default function Unlocker(props: PropTypes) {
-	useEffect(() => {
-		if (props.success) {
-			fillUnlockers();
-			return;
-		}
-		animateAnswer(props.secret);
-	}, [props.secret, props.success]);
+    useEffect(() => {
+        if (props.success) {
+            fillUnlockers();
+            return;
+        }
+        animateAnswer(props.secret);
+    }, [props.secret, props.success]);
 
-	const onClickHandler = (input: string) => {
-		const unlockerElement = document.getElementById('unlocker' + input);
-		if (unlockerElement) {
-			if (
-				unlockerElement.className === 'unlocker-item-filled' ||
-				unlockerElement.className === 'unlocker-item disabled'
-			) {
-				return;
-			}
-			unlockerElement.className = 'unlocker-item-filled';
-			props.addInput(input);
-		}
-	};
+    const onClickHandler = (input: string) => {
+        const unlockerElement = document.getElementById('unlocker' + input);
+        if (unlockerElement) {
+            if (
+                unlockerElement.className === 'unlocker-item-filled' ||
+                unlockerElement.className === 'unlocker-item disabled'
+            ) {
+                return;
+            }
+            unlockerElement.className = 'unlocker-item-filled';
+            props.addInput(input);
+        }
+    };
 
-	return (
-		<div
-			key={props.secret}
-			className={
-				props.success ? 'unlocker-container-exit' : 'unlocker-container'
-			}
-		>
-			{unlockers.map((item) => {
-				return (
-					<div
-						id={'unlocker' + item}
-						className='unlocker-item disabled'
-						onClick={() => onClickHandler(item)}
-					></div>
-				);
-			})}
-		</div>
-	);
+    return (
+        <div
+            key={props.secret}
+            className={
+                props.success ? 'unlocker-container-exit' : 'unlocker-container'
+            }
+        >
+            {unlockers.map((item) => {
+                return (
+                    <div
+                        id={'unlocker' + item}
+                        key={item}
+                        className='unlocker-item disabled'
+                        onClick={() => onClickHandler(item)}
+                    ></div>
+                );
+            })}
+        </div>
+    );
 }
