742a3f2e53a750455c5eed649c94817ace0b766c
MINK <76023265+kk-min@users.noreply.github.com>
2022-09-27 12:35:46 +0800

    Add About me content and styling

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -35,62 +35,30 @@ export default function AboutMe(props: PropTypes) {
 				</button>
 			</div>
 			<div className='about-me-content' id='about-me-content'>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-				sed eros lorem. Nullam vitae est in ante pellentesque dictum.
-				Integer orci metus, consectetur et placerat vel, bibendum vel
-				ex. Morbi non eros a libero vestibulum fermentum ut vel orci.
-				Donec scelerisque, massa eu ornare congue, lectus orci hendrerit
-				turpis, in tincidunt neque sem ullamcorper diam. Proin dignissim
-				accumsan laoreet. <br />
-				<br />
-				Pellentesque viverra metus a sapien gravida, a tincidunt tellus
-				tristique. Etiam faucibus in est in dapibus. Praesent fringilla
-				nisi et ipsum hendrerit interdum. Fusce non leo scelerisque,
-				semper magna non, dictum lacus. Vestibulum congue felis a nibh
-				facilisis porta. Class aptent taciti sociosqu ad litora torquent
-				per conubia nostra, per inceptos himenaeos. <br />
-				<br />
-				Fusce felis tellus, scelerisque et vehicula non, pellentesque
-				sed nunc. Praesent fringilla vehicula hendrerit. Suspendisse eu
-				eros quis justo tempor blandit. Vestibulum at ante enim.
-				Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-				posuere cubilia curae; Proin ante ligula, suscipit a imperdiet
-				non, malesuada eget felis. <br />
-				<br />
-				Integer porta feugiat dui, sed convallis justo. Quisque pharetra
-				augue et ultrices hendrerit. Vivamus vestibulum velit est,
-				sollicitudin faucibus sapien pretium eget. Morbi id molestie
-				sem. Sed vitae rhoncus mi. Fusce eu lectus enim. Praesent
-				sollicitudin placerat velit ac pellentesque. Pellentesque
-				lobortis bibendum nibh, et tempus tortor commodo finibus. <br />
-				<br />
-				Sed ultrices eros ac sollicitudin tincidunt. Donec sollicitudin
-				malesuada magna, vitae dapibus augue feugiat in. Suspendisse nec
-				neque vestibulum, ultrices purus non, luctus massa. Quisque a
-				elementum magna. Nulla congue blandit tortor sit amet fermentum.{' '}
+				Creating things has always been a big part of my life. When I
+				was young, I enjoyed building my own lego structures to stories
+				during playtime. When I was in highschool, I was deeply enamored
+				by music and the creative process behind composition, and the
+				way{' '}
+				<a href='https://en.wikipedia.org/wiki/Digital_audio_workstation'>
+					software augments it.
+				</a>{' '}
+				This eventually led me to study Computer Science and learn
+				software development. Now, I aspire to create
+				technology/software to make a difference in the world.
 				<br />
 				<br />
-				Nam imperdiet augue sit amet ex volutpat, at aliquet orci
-				scelerisque. Sed cursus odio velit, in placerat velit
-				ullamcorper ut. Phasellus libero nulla, tempus ut fringilla ut,
-				consectetur vitae velit. Aliquam erat volutpat. Interdum et
-				malesuada fames ac ante ipsum primis in faucibus. Donec
-				scelerisque non urna vitae sollicitudin. <br />
+				I have experience in both Frontend (Reactjs,
+				Javascript/Typescript, CSS) and Backend (Anroid development,
+				Java, Kotlin, C/C++) experience as well as scripting (Python,
+				VBA). I'm also always looking out to learn and explore new
+				technologiesl; see Projects for more details.
 				<br />
-				In hac habitasse platea dictumst. Pellentesque non suscipit
-				sapien. Donec at finibus risus. Pellentesque aliquam sem vitae
-				ligula vehicula, sed auctor ligula egestas. Curabitur sit amet
-				vulputate felis, sit amet ullamcorper neque. Praesent interdum
-				metus velit, non lacinia lacus finibus et. Donec laoreet, velit
-				vitae eleifend consequat, est lacus vulputate enim, vitae
-				elementum arcu mauris quis mauris. Nunc consequat libero ut
-				felis luctus, non iaculis velit suscipit. <br />
 				<br />
-				Nunc in erat sit amet lacus auctor ullamcorper quis ac enim.
-				Pellentesque sit amet laoreet massa. Aenean dignissim vulputate
-				mauris. Integer gravida sodales massa vel egestas. Mauris sit
-				amet diam vitae nunc dignissim vulputate. Phasellus non ornare
-				tellus. Duis consequat convallis dolor et molestie.
+				If you have an idea for which you think we can collaborate and
+				work together, feel free to reach out to me via the Contact tab!
+				<br />
+				<br />
 			</div>
 		</div>
 	);
