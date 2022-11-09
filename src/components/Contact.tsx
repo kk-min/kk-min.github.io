c53ad2f8c5f2859c57f045889c7f9b4bb9e4fe37
mink0003 <76023265+kk-min@users.noreply.github.com>
2022-11-09 09:15:17 +0800

    Modify email in Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,155 +7,171 @@ import reset_arrow from '../assets/reset_arrow.png';
 const randomizerArray = ['1', '2', '3', '4'];
 
 const getRandomSecret = (sourceArray: string[]) => {
-	let secret: string = '';
-	let tempArray = [...sourceArray];
-	while (tempArray.length > 0) {
-		let index = Math.floor(Math.random() * tempArray.length);
-		let randomItem = tempArray[index];
-		secret += randomItem;
-		tempArray.splice(index, 1); // Remove selected item from array
-	}
-	return secret;
+    let secret: string = '';
+    let tempArray = [...sourceArray];
+    while (tempArray.length > 0) {
+        let index = Math.floor(Math.random() * tempArray.length);
+        let randomItem = tempArray[index];
+        secret += randomItem;
+        tempArray.splice(index, 1); // Remove selected item from array
+    }
+    return secret;
 };
 
 export default function Contact() {
-	const [secret, setSecret] = useState(getRandomSecret(randomizerArray));
-	const [input, setInput] = useState('');
-	const [lockState, setLockState] = useState('default');
-	const [isAnimating, setIsAnimating] = useState(true);
+    const [secret, setSecret] = useState(getRandomSecret(randomizerArray));
+    const [input, setInput] = useState('');
+    const [lockState, setLockState] = useState('default');
+    const [isAnimating, setIsAnimating] = useState(true);
 
-	useEffect(() => {
-		const lockElement = document.getElementById('lock');
-		const unlockerElement = document.getElementById('unlocker');
-		const contactElement = document.getElementById('contact-card');
+    useEffect(() => {
+        const lockElement = document.getElementById('lock');
+        const unlockerElement = document.getElementById('unlocker');
+        const contactElement = document.getElementById('contact-card');
 
-		if (lockState === 'open') {
-			setTimeout(() => {
-				if (lockElement) {
-					lockElement.style.display = 'none';
-				}
-				if (unlockerElement) {
-					unlockerElement.style.display = 'none';
-				}
-				if (contactElement) {
-					contactElement.className = 'contact-info';
-				}
-			}, 2000);
-			return;
-		} else {
-			setTimeout(() => {
-				setIsAnimating(false);
-			}, 5000);
-		}
-	}, [lockState]);
+        if (lockState === 'open') {
+            setTimeout(() => {
+                if (lockElement) {
+                    lockElement.style.display = 'none';
+                }
+                if (unlockerElement) {
+                    unlockerElement.style.display = 'none';
+                }
+                if (contactElement) {
+                    contactElement.className = 'contact-info';
+                }
+            }, 2000);
+            return;
+        } else {
+            setTimeout(() => {
+                setIsAnimating(false);
+            }, 5000);
+        }
+    }, [lockState]);
 
-	useEffect(() => {
-		if (lockState === 'open') {
-			return;
-		}
+    useEffect(() => {
+        if (lockState === 'open') {
+            return;
+        }
 
-		const resetElement = document.getElementById('reset-button');
-		if (!isAnimating) {
-			if (resetElement) {
-				resetElement.className = 'reset-button enabled';
-			}
-		}
-	}, [isAnimating, lockState]);
+        const resetElement = document.getElementById('reset-button');
+        if (!isAnimating) {
+            if (resetElement) {
+                resetElement.className = 'reset-button enabled';
+            }
+        }
+    }, [isAnimating, lockState]);
 
-	const addInput = (input: string) => {
-		setInput((prev) => prev + input);
-	};
+    const addInput = (input: string) => {
+        setInput((prev) => prev + input);
+    };
 
-	const resetHandler = () => {
-		if (isAnimating) {
-			return;
-		}
+    const resetHandler = () => {
+        if (isAnimating) {
+            return;
+        }
 
-		// Clear all setTimeOut functions:
-		var timeOutID = window.setTimeout(function () {}, 0);
-		while (timeOutID--) {
-			window.clearTimeout(timeOutID);
-		}
+        // Clear all setTimeOut functions:
+        var timeOutID = window.setTimeout(function () {}, 0);
+        while (timeOutID--) {
+            window.clearTimeout(timeOutID);
+        }
 
-		const resetElement = document.getElementById('reset-button');
-		if (resetElement) {
-			if (resetElement.className === 'reset-button disabled') {
-				return;
-			}
+        const resetElement = document.getElementById('reset-button');
+        if (resetElement) {
+            if (resetElement.className === 'reset-button disabled') {
+                return;
+            }
 
-			resetElement.classList.add('rotate');
-			resetElement.classList.remove('enabled');
-			resetElement.classList.add('disabled');
+            resetElement.classList.add('rotate');
+            resetElement.classList.remove('enabled');
+            resetElement.classList.add('disabled');
 
-			const lockContainerElement = document.getElementById('lock');
-			const lockImageElement = document.getElementById('lock-image');
-			const unlockerElement = document.getElementById('unlocker');
-			if (lockContainerElement) {
-				lockContainerElement.style.removeProperty('display');
-			}
+            const lockContainerElement = document.getElementById('lock');
+            const lockImageElement = document.getElementById('lock-image');
+            const unlockerElement = document.getElementById('unlocker');
+            if (lockContainerElement) {
+                lockContainerElement.style.removeProperty('display');
+            }
 
-			if (unlockerElement) {
-				unlockerElement.style.removeProperty('display');
-			}
+            if (unlockerElement) {
+                unlockerElement.style.removeProperty('display');
+            }
 
-			setSecret((prev) => getRandomSecret(randomizerArray));
-			setInput((prev) => '');
-			setLockState((prev) => 'default');
-			setIsAnimating((prev) => true);
+            setSecret((prev) => getRandomSecret(randomizerArray));
+            setInput((prev) => '');
+            setLockState((prev) => 'default');
+            setIsAnimating((prev) => true);
 
-			if (lockImageElement) {
-				console.log('Resetting lock to initial');
-				lockImageElement.classList.add('initial');
-				setTimeout(() => {
-					lockImageElement.className = 'lock';
-				}, 1000);
-			}
-		}
-		setTimeout(() => {
-			resetElement?.classList.remove('rotate');
-			resetElement?.classList.remove('disabled');
-			setIsAnimating((prev) => false);
-		}, 5000);
-	};
+            if (lockImageElement) {
+                console.log('Resetting lock to initial');
+                lockImageElement.classList.add('initial');
+                setTimeout(() => {
+                    lockImageElement.className = 'lock';
+                }, 1000);
+            }
+        }
+        setTimeout(() => {
+            resetElement?.classList.remove('rotate');
+            resetElement?.classList.remove('disabled');
+            setIsAnimating((prev) => false);
+        }, 5000);
+    };
 
-	return (
-		<div className='content'>
-			<div className='title-container'>
-				<h1 className='tab-title'>Contact</h1>
-				<img
-					id='reset-button'
-					className='reset-button disabled'
-					onClick={resetHandler}
-					src={reset_arrow}
-					alt='reset'
-				/>
-			</div>
-			<Lock
-				secret={secret}
-				input={input}
-				lockState={lockState}
-				setLockState={setLockState}
-			/>
-			<Unlocker
-				secret={secret}
-				addInput={addInput}
-				lockState={lockState}
-			/>
-			{lockState === 'open' ? (
-				<div className='contact-info-invisible' id='contact-card'>
-					<ProjectItem
-						projectTitle='Min Kabar Kyaw'
-						projectDescription=''
-						projectIcon=''
-						projectRole='Software Developer'
-						projectLink=''
-						projectBulletPoints={[
-							'Email: minkabarkyaw@gmail.com',
-							'LinkedIn: https://www.linkedin.com/in/minkk/',
-						]}
-					/>
-				</div>
-			) : null}
-		</div>
-	);
+    return (
+        <div className='content'>
+            <div className='title-container'>
+                <h1 className='tab-title'>Contact</h1>
+                <img
+                    id='reset-button'
+                    className='reset-button disabled'
+                    onClick={resetHandler}
+                    src={reset_arrow}
+                    alt='reset'
+                />
+            </div>
+            <Lock
+                secret={secret}
+                input={input}
+                lockState={lockState}
+                setLockState={setLockState}
+            />
+            <Unlocker
+                secret={secret}
+                addInput={addInput}
+                lockState={lockState}
+            />
+            {lockState === 'open' ? (
+                <div className='contact-info-invisible' id='contact-card'>
+                    <ProjectItem
+                        projectTitle='Min Kabar Kyaw'
+                        projectDescription=''
+                        projectIcon=''
+                        projectRole='Software Developer'
+                        projectLink=''
+                        projectBulletPoints={[
+                            'E' +
+                                'ma' +
+                                'il:' +
+                                ' ' +
+                                'mi' +
+                                'n' +
+                                'kab' +
+                                'a' +
+                                'r' +
+                                'ky' +
+                                'aw' +
+                                '(at)' +
+                                'g' +
+                                'mai' +
+                                'l' +
+                                '.c' +
+                                'om',
+                            'LinkedIn: https://www.linkedin.com/in/minkk/',
+                        ]}
+                    />
+                </div>
+            ) : null}
+        </div>
+    );
 }
