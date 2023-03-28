d18ab3ce27c444d3c3599c963979e0cbc55c1dea
kk-min <76023265+kk-min@users.noreply.github.com>
2023-03-28 12:05:46 +0800

    Add TabState interface to Store.tsx

--- src/data/Store.tsx
+++ src/data/Store.tsx
@@ -1,6 +1,11 @@
 import { create } from 'zustand'
 
-export const useTabStore = create((set) => ({
+export interface TabState {
+	activeTab: string
+	setActiveTab: (tab: string) => void
+}
+
+export const useTabStore = create<TabState>((set) => ({
 	activeTab: "About Me",
 	setActiveTab: (tab: string) => set({ activeTab: tab }),
 }))
