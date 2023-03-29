85ee5a6225b361ee1be2b1bbc923e98c11043972
kk-min <76023265+kk-min@users.noreply.github.com>
2023-03-29 09:47:59 +0800

    Add PuzzleState and usePuzzleStore to Store.tsx

--- src/data/Store.tsx
+++ src/data/Store.tsx
@@ -4,8 +4,40 @@ export interface TabState {
 	activeTab: string
 	setActiveTab: (tab: string) => void
 }
+export interface PuzzleState {
+	secret: string
+	input: string
+	lockStatus: string
+	isAnimating: boolean
+	resetSecret: () => void
+	setInput: (input: string) => void
+	setIsAnimating: (isAnimating: boolean) => void
+}
 
 export const useTabStore = create<TabState>((set) => ({
 	activeTab: "About Me",
 	setActiveTab: (tab: string) => set({ activeTab: tab }),
 }))
+
+export const usePuzzleStore = create<PuzzleState>((set, get) => ({
+	secret: getRandomSecret(randomizerArray),
+	input: "",
+	lockStatus: "default",
+	isAnimating: true,
+	resetSecret: () => set({ secret: getRandomSecret(randomizerArray) }),
+	setInput: (input: string) => set({ input, lockStatus: input === get().secret ? "open" : input.length === 4 ? "error" : "default" }),
+	setIsAnimating: (isAnimating: boolean) => set({ isAnimating }),
+}))
+
+const randomizerArray = ['1', '2', '3', '4'];
+const getRandomSecret = (sourceArray: string[]) => {
+	let secret: string = '';
+	let tempArray = [...sourceArray];
+	while (tempArray.length > 0) {
+		let index = Math.floor(Math.random() * tempArray.length);
+		let randomItem = tempArray[index];
+		secret += randomItem;
+		tempArray.splice(index, 1); // Remove selected item from array
+	}
+	return secret;
+};
