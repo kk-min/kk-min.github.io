d5ae87dbb47d35ab687dd29c1ee9aafb6dcea493
kk-min <76023265+kk-min@users.noreply.github.com>
2023-03-29 10:45:18 +0800

    Add input update when unlockers are clicked

--- src/components/Unlocker.tsx
+++ src/components/Unlocker.tsx
@@ -1,90 +1,90 @@
-import React, { useEffect } from "react";
-
-export interface PropTypes {
-  secret: string;
-  addInput: (input: string) => void;
-  lockState: string;
-}
+import { useEffect } from "react";
+import { usePuzzleStore } from "../data/Store";
 
 const unlockers = ["1", "2", "3", "4"];
 const timer = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 const animateAnswer = async (secret: string) => {
-  resetUnlockers("disabled");
-  await timer(1000);
-  // Fill the unlocker items in sequence of the secret:
-  for (const c of secret) {
-    const unlockerElement = document.getElementById("unlocker" + c);
-    if (unlockerElement) {
-      unlockerElement.className = "unlocker-item-filled";
-      await timer(1000);
-    }
-  }
-  resetUnlockers("enabled");
+	resetUnlockers("disabled");
+	await timer(1000);
+	// Fill the unlocker items in sequence of the secret:
+	for (const c of secret) {
+		const unlockerElement = document.getElementById("unlocker" + c);
+		if (unlockerElement) {
+			unlockerElement.className = "unlocker-item-filled";
+			await timer(1000);
+		}
+	}
+	resetUnlockers("enabled");
 };
 
 const resetUnlockers = (status: string) => {
-  // Un-fill the unlocker items
-  for (const c of unlockers) {
-    const unlockerElement = document.getElementById("unlocker" + c);
-    if (unlockerElement) {
-      unlockerElement.className = "unlocker-item " + status;
-    }
-  }
+	// Un-fill the unlocker items
+	for (const c of unlockers) {
+		const unlockerElement = document.getElementById("unlocker" + c);
+		if (unlockerElement) {
+			unlockerElement.className = "unlocker-item " + status;
+		}
+	}
 };
 
 const fillUnlockers = () => {
-  for (const c of unlockers) {
-    const unlockerElement = document.getElementById("unlocker" + c);
-    if (unlockerElement) {
-      unlockerElement.className = "unlocker-item-filled";
-    }
-  }
+	for (const c of unlockers) {
+		const unlockerElement = document.getElementById("unlocker" + c);
+		if (unlockerElement) {
+			unlockerElement.className = "unlocker-item-filled";
+		}
+	}
 };
 
-export default function Unlocker(props: PropTypes) {
-  useEffect(() => {
-    if (props.lockState === "open" || props.lockState === "error") {
-      fillUnlockers();
-      return;
-    }
-    animateAnswer(props.secret);
-  }, [props.secret, props.lockState]);
+export default function Unlocker() {
+	const lockStatus = usePuzzleStore((state) => state.lockStatus);
+	const secret = usePuzzleStore((state) => state.secret);
+	const input = usePuzzleStore((state) => state.input);
+	const setInput = usePuzzleStore((state) => state.setInput);
 
-  const onClickHandler = (input: string) => {
-    const unlockerElement = document.getElementById("unlocker" + input);
-    if (unlockerElement) {
-      if (
-        unlockerElement.className === "unlocker-item-filled" ||
-        unlockerElement.className === "unlocker-item disabled"
-      ) {
-        return;
-      }
-      unlockerElement.className = "unlocker-item-filled";
-      props.addInput(input);
-    }
-  };
+	useEffect(() => {
+		if (lockStatus === "open" || lockStatus === "error") {
+			fillUnlockers();
+			return;
+		}
+		animateAnswer(secret);
+	}, [secret, lockStatus]);
 
-  return (
-    <div
-      id="unlocker"
-      key={props.secret}
-      className={
-        props.lockState === "open"
-          ? "unlocker-container-exit"
-          : "unlocker-container"
-      }
-    >
-      {unlockers.map((item) => {
-        return (
-          <div
-            id={"unlocker" + item}
-            key={item}
-            className="unlocker-item disabled"
-            onClick={() => onClickHandler(item)}
-          ></div>
-        );
-      })}
-    </div>
-  );
+	const onClickHandler = (id: string) => {
+		const unlockerElement = document.getElementById("unlocker" + id);
+		if (unlockerElement) {
+			if (
+				unlockerElement.className === "unlocker-item-filled" ||
+				unlockerElement.className === "unlocker-item disabled"
+			) {
+				return;
+			}
+			unlockerElement.className = "unlocker-item-filled";
+			setInput(input + id)
+		}
+	};
+
+	return (
+		<div
+			id="unlocker"
+			key={secret}
+			className={
+				lockStatus === "open"
+					? "unlocker-container-exit"
+					: "unlocker-container"
+			}
+		>
+			{unlockers.map((item) => {
+				return (
+					<div
+						id={"unlocker" + item}
+						key={item}
+						className="unlocker-item disabled"
+						onClick={() => onClickHandler(item)}
+					></div>
+				);
+			})}
+		</div>
+	);
 }
